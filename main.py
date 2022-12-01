9a4d1710fc2ca7c5f86807b265e21efa1c648980
DannyDeng <1041422509@qq.com>
2022-12-01 13:04:42 -0800

    remove unused func

--- main.py
+++ main.py
@@ -72,29 +72,29 @@ class GCN(torch.nn.Module):
                 x = F.dropout(x, p=0.5, training=self.training)
         return x.log_softmax(dim=-1)
 
-    @torch.no_grad()
-    def inference(self, x_all, device, subgraph_loader):
-        pbar = tqdm(total=x_all.size(0) * self.num_layers)
-        pbar.set_description('Evaluating')
-
-        for i in range(self.num_layers):
-            xs = []
-            for batch_size, n_id, adj in subgraph_loader:
-                edge_index, _, size = adj.to(device)
-                x = x_all[n_id].to(device)
-                x_target = x[:size[1]]
-                x = self.convs[i]((x, x_target), edge_index)
-                if i != self.num_layers - 1:
-                    x = F.relu(x)
-                xs.append(x.cpu())
-
-                pbar.update(batch_size)
-
-            x_all = torch.cat(xs, dim=0)
-
-        pbar.close()
-
-        return x_all
+    # @torch.no_grad()
+    # def inference(self, x_all, device, subgraph_loader):
+    #     pbar = tqdm(total=x_all.size(0) * self.num_layers)
+    #     pbar.set_description('Evaluating')
+    #
+    #     for i in range(self.num_layers):
+    #         xs = []
+    #         for batch_size, n_id, adj in subgraph_loader:
+    #             edge_index, _, size = adj.to(device)
+    #             x = x_all[n_id].to(device)
+    #             x_target = x[:size[1]]
+    #             x = self.convs[i]((x, x_target), edge_index)
+    #             if i != self.num_layers - 1:
+    #                 x = F.relu(x)
+    #             xs.append(x.cpu())
+    #
+    #             pbar.update(batch_size)
+    #
+    #         x_all = torch.cat(xs, dim=0)
+    #
+    #     pbar.close()
+    #
+    #     return x_all
 
 
 class GCN2(torch.nn.Module):
